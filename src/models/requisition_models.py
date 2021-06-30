dbf6fa80903a91665bc3bd6b6378c4131dd8d078
João Paulo Antunes <jpantunesdesouza@gmail.com>
2021-06-30 16:27:22 -0300

    feat: Upgrade models

--- src/models/requisition_models.py
+++ src/models/requisition_models.py
@@ -1,15 +1,16 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Float
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, TIMESTAMP, Float
 from sqlalchemy.orm import relationship
+# import datetime
+from sqlalchemy.sql import func
 
 from ..database import Base
-import datetime
 
 class Requisition(Base):
     __tablename__ = "requisitions"
 
     reqn = Column(Integer, primary_key=True, index=True) # alterar o tipo para UUID
-    file_url = Column(String, unique=True, index=True)
-    last_modified = Column(DateTime, onupdate=datetime.datetime.now) # define to be populated with datetime.now()
+    file_url = Column(String, index=True)
+    time_updated = Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
     progress_level = Column(Float)
     is_free = Column(Boolean)
     owner_id = Column(Integer, ForeignKey("users.id"))
